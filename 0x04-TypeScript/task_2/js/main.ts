deedbe4bef903717a57301381c5d9244918c83f1
mohamed keytsane <osamakeysane20@gmail.com>
2025-09-14 21:11:50 +0300

    removed space

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -78,10 +78,11 @@ function executeWork(employee: Director | Teacher): string {
 console.log(executeWork(createEmployee(200))); // Getting to work
 console.log(executeWork(createEmployee(1000))); // Getting to director tasks
 
-/// Task 7: String literal types
+// Task 7: String literal types
 type Subjects = "Math" | "History";
 
-function teachClass(todayClass: Subjects): string {
+// prettier-ignore
+function teachClass(todayClass:Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
   } else {
